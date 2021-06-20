d40284e9efb6c5ae2e2447e77b3276467f91bedd
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-06-20 10:24:46 -0300

    merge completo

--- briondrinks/app/views/admin/produtos.view.php
+++ briondrinks/app/views/admin/produtos.view.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<<<<<<< HEAD
     <?php require('headADM.php'); ?>
     <title>Lista de Produtos</title>
 </head>
@@ -14,49 +13,6 @@
     <h2 class="titulo-pagina mt-5"><b>LISTA DE PRODUTOS</b></h2>
     <div class="linhaHorizontal mb-5"></div>
 
-=======
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../../public/css/css-adm.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="icon" type="imagem/png" href="../img/logo.png" />
-    <link rel="stylesheet" href="../../../public/css/styles.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Produtos</title>
-</head>
-<body>
-<!-- NAVBAR -->
-    <div id="navbar" class="navbar cor-navbar">
-        <div class="logo-esq">
-            <img src="../../../public/img/5.png" alt="Logo da empresa Brion Drinks e Coquetelaria">
-        </div>
-        
-        <ul>
-            <li><a href="view_adm_usuarios.html">Usuários</a></li>
-            <li><a href="admprodutos.html">Produtos</a></li>
-            <li><a href="view_adm_categoria.html">Categorias</a></li>
-            <li><a href="view_login">Sair</a></li>
-            <li id="menu-icon" class="menu-icon" onclick="openNav()">&#x1F378; Menu</li>
-        </ul>
-
-        <div class="logo-dir">
-            <img src="../../../public/img/5.png" alt="Logo da empresa Brion Drinks e Coquetelaria">
-        </div>
-    </div>
-
-
-    <h2 class="titulo-pagina mt-5"><b>LISTA DE PRODUTOS</b></h2>
-    <div class="linhaHorizontal mb-5"></div>
-
-<!-- PHP -->
-
-
->>>>>>> main
 <!-- BOTÃO ADICIONAR -->
 <div class="container botao-adicionar mb-3" data-toggle="modal" data-target="#modalAdicionar">
     <button type="button" class="btn cor-botoes">Adicionar</button>
@@ -79,11 +35,7 @@
             <?php foreach ($produtos as $produto) : ?>
             <tr>
                 
-<<<<<<< HEAD
                 <th scope="row"><img src="../../../public/img/<?= $produto->foto ?>" alt="Foto do Usuário" class="foto-tabela"></th>
-=======
-                <th scope="row"><img src="../../../public/img/batida.png" alt="Foto do Usuário" class="foto-tabela"></th>
->>>>>>> main
                 <td class="align-middle"><?= $produto->nome ?></td>
                 <td class="align-middle colunasInvisiveis"><?= $produto->descricao ?></td>
                 <td class="align-middle colunasInvisiveis"><?= $produto->preco ?></td>
